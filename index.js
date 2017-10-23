aec8dc7357b063fec7766e8ac6141f7e93cda725
96qbhy <96qbhy@gmail.com>
2017-10-23 17:58:10 +0800

    init

---
+++ index.js
@@ -0,0 +1,29 @@
+/*!
+ Copyright (c) 2017 96qbhy.
+ Licensed under the MIT License (MIT)
+ */
+/* global define */
+
+(function () {
+    'use strict';
+
+    var axios = window.axios || require('axios');
+
+    function uploadImg() {
+        var data = new FormData();
+        data.append('smfile', file);
+        data.append('ssl', true);
+        return axios.post('https://sm.ms/api/upload', data).then(data => data.data);
+    }
+
+    if (typeof module !== 'undefined' && module.exports) {
+        module.exports = uploadImg;
+    } else if (typeof define === 'function' && typeof define.amd === 'object' && define.amd) {
+        // register as 'uploadImg', consistent with npm package name
+        define('uploadImg', [], function () {
+            return uploadImg;
+        });
+    } else {
+        window.uploadImg = uploadImg;
+    }
+}());
